306847d272a5bbce5f420007ba2d4dc46b779490
Nishant Kumar Verma <nisgyan@gmail.com>
2022-10-04 22:38:07 +0200

    - Started working on automator website

---
+++ nkv.GetAutomator/Pages/Website/Welcome.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace nkv.GetAutomator.Pages.Website
+{
+    public partial class Welcome : ComponentBase
+    {
+
+    }
+}
---
+++ nkv.GetAutomator/Shared/AppBarLandingPage.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace nkv.GetAutomator.Shared
+{
+    public partial class AppBarLandingPage : ComponentBase
+    {
+
+    }
+}
